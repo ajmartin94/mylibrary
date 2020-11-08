e1fdde02bb50aacbe3dd27911f9bbefcbaf64f74
Andrew Martin <martinandrew2@johndeere.com>
2020-11-08 17:54:25 -0600

    Added CORS header package

--- mylibrary/mylibrary-react/src/App.js
+++ mylibrary/mylibrary-react/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import axios from 'axios';
+import {useState} from 'react';
 
 function App() {
+  const [data,setData] = useState(null)
 
-  handleClick = () => {
-    
+  const handleClick = () => {
+    axios.get('http://localhost:8000/library/select/OL27448W')
+    .then(resp => {
+      setData(resp.data)
+    })
   }
 
   return (
     <div className="App">
       <h1>Yooo</h1>
-      <button onClick={}></button>
+      <button onClick={handleClick}>Click me!</button>
     </div>
   );
 }
