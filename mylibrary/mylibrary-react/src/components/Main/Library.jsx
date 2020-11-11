365f35dd6c8096bc34b588d7153413fda1072aca
Andrew Martin <martinandrew2@johndeere.com>
2020-11-11 14:48:38 -0600

    Added library tabs

--- mylibrary/mylibrary-react/src/components/Main/Library.jsx
+++ mylibrary/mylibrary-react/src/components/Main/Library.jsx
@@ -1,9 +1,34 @@
 import React,{useState,useEffect} from 'react';
 
 function Library(props) {
+    console.log(props.libraryData)
     return (
         <div>
-            
+            <ul className='nav nav-tabs' id='libraryTabs' role='tablist'>
+                {props.libraryData.map((library,index) => {
+                    return <li className='nav-item' role='presentation'>
+                        <a 
+                            className={index===0 ? 'nav-link active' : 'nav-link'}
+                            data-toggle='tab'
+                            href={'#tab'+index}
+                            role='tab'
+                            aria-controls={'tab'+index}
+                            aria-selected={index===0 ? 'true' : 'false'}
+                        >{library.name}</a>
+                    </li>
+                })}
+            </ul>
+            <div className='tab-content' id='libraryContent'>
+                {props.libraryData.map((library,index)=>{
+                    return <div 
+                        className={index===0 ? 'tab-pane fade show active' : 'tab-pane fade'}
+                        id={'tab'+index}
+                        role='tabpanel'
+                        aria-labelledby={'tab'+index}
+                    >{library.name}</div>
+                })
+                }
+            </div>
         </div>
     )
 }
