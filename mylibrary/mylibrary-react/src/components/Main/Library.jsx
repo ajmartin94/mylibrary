1f26c43da961ff458aad3661a23b17733d12c0aa
Andrew Martin <martinandrew2@johndeere.com>
2020-11-12 15:41:15 -0600

    Changing the library view page

--- mylibrary/mylibrary-react/src/components/Main/Library.jsx
+++ mylibrary/mylibrary-react/src/components/Main/Library.jsx
@@ -5,6 +5,9 @@ import AddLibrary from './AddLibrary'
 
 const LibraryDiv = styled.div`
     width: 75%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
 const Tab = styled.div`
@@ -42,37 +45,39 @@ function Library(props) {
             <LibraryDiv>
                 <ul className='nav nav-tabs' id='libraryTabs' role='tablist'>
                     {props.libraryData.map((library,index) => {
-                        return <li className='nav-item' role='presentation'>
-                            <Tab
-                                className={library.id===props.activeLibraryID ? 'nav-link active' : 'nav-link'}
-                                
-                            >  
-                                <div 
-                                    data-toggle='tab'
-                                    role='tab'
-                                    aria-controls={'tab'+index}
-                                    aria-selected={index===0 ? 'true' : 'false'}
-                                    onClick={()=>handleClick(library.id)}
-                                >
-                                {library.name} 
-                                </div>
-                                
-                                <TrashIcon 
-                                    width="1em" 
-                                    height="1em" 
-                                    viewBox="0 0 16 16" 
-                                    class="bi bi-trash" 
-                                    fill="currentColor" 
-                                    xmlns="http://www.w3.org/2000/svg" 
-                                    data-toggle='modal'
-                                    data-target='#confirmModal'
-                                    onClick={()=>setPendDelete(library.id)}
-                                >
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                </TrashIcon>
-                            </Tab>
-                        </li>
+                        return (
+                            <li key={index} className='nav-item' role='presentation'>
+                                <Tab
+                                    className={library.id===props.activeLibraryID ? 'nav-link active' : 'nav-link'}
+                                    
+                                >  
+                                    <div 
+                                        data-toggle='tab'
+                                        role='tab'
+                                        aria-controls={'tab'+index}
+                                        aria-selected={index===0 ? 'true' : 'false'}
+                                        onClick={()=>handleClick(library.id)}
+                                    >
+                                    {library.name} 
+                                    </div>
+                                    
+                                    <TrashIcon 
+                                        width="1em" 
+                                        height="1em" 
+                                        viewBox="0 0 16 16" 
+                                        class="bi bi-trash" 
+                                        fill="currentColor" 
+                                        xmlns="http://www.w3.org/2000/svg" 
+                                        data-toggle='modal'
+                                        data-target='#confirmModal'
+                                        onClick={()=>setPendDelete(library.id)}
+                                    >
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                    </TrashIcon>
+                                </Tab>
+                            </li>
+                        )
                     })}
                     <li className='nav-item' role='presentation'>
                         <Tab 
--- mylibrary/mylibrary-react/src/components/Main/LibraryContent.jsx
+++ mylibrary/mylibrary-react/src/components/Main/LibraryContent.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styled from 'styled-components';
 
 const BookCard = styled.div`
     width: 25%;
+    cursor: pointer;
 `
 
 function LibraryContent(props) {
+    const [activeBook,setActiveBook] = useState(null)
+
     return (
         <>
-        {props.books.map(book => {
-            return <BookCard className='card'>
-                <img 
-                    src={`http://covers.openlibrary.org/b/id/${book.data.covers[0]}-M.jpg`} 
-                    className='card-img-top'
-                    alt={`cover for ${book.title}`}    
-                />
-                <div className='card-body'>
-                    <h5 className='card-title'>{book.title}</h5>
-                    <p className='card-text'>{Object.keys(book.data.description).length > 0 ? book.data.description.value : book.data.description}</p>
+            {props.books.map(book => {
+                return <BookCard className='card'>
+                    <img 
+                        src={`http://covers.openlibrary.org/b/id/${book.data.covers[0]}-L.jpg`} 
+                        className='card-img-top'
+                        alt={`cover for ${book.title}`}  
+                        data-toggle='modal' 
+                        data-target='detailsModal' 
+                        onClick={()=>setActiveBook(book)}  
+                    />
+                    <div className='card-body'>
+                        <h5 className='card-title'>{book.title}</h5>
+                        {/* <p className='card-text'>{typeof book.data.description !== 'string' && Object.keys(book.data.description).length > 0 ? book.data.description.value : book.data.description}</p> */}
+                    </div>
+                </BookCard>
+            })}
+            {/* <div className="modal show fade" id="detailsModal" tabindex="-1" aria-labelledby="detailsModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="detailsModalLabel">{activeBook.title}</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="media">
+                                <img src={`http://covers.openlibrary.org/b/id/${activeBook.data.covers[0]}-M.jpg`} className="mr-3" alt="..." />
+                                <div className="media-body">
+                                    {activeBook.data.description}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </BookCard>
-        })}
+            </div> */}
         </>
     )
 }
